aee7435ac73f3812927e7973f4c95c00a4ccc0e4
Suliman Awad <sulimanawadstudy@gmail.com>
2023-01-12 02:52:10 +0300

    .

--- My Templates/Matrices/Vec_Matrices.cpp
+++ My Templates/Matrices/Vec_Matrices.cpp
@@ -47,42 +47,42 @@ ll mat_trace_mod(mll a){
 }
 
 /// --- Sum Two Matrix  --- ///
-mll mat_add(mll a, mll b){
-    mll res = zero_mat(a.size(), a[0].size());
-    for(int i = 0 ; i < a.size() ; i++)
-        for(int j = 0 ; j < a[0].size() ; j++)
-            res[i][j] = a[i][j] + b[i][j];
+mll mat_add(mll a1, mll a2){
+    mll res = zero_mat(a1.size(), a1[0].size());
+    for(int i = 0 ; i < a1.size() ; i++)
+        for(int j = 0 ; j < a1[0].size() ; j++)
+            res[i][j] = a1[i][j] + a2[i][j];
     return res;
 }
 
 /// --- Sum Two Matrix With MOD  --- ///
-mll mat_add_mod(mll a, mll b){
-    mll res = zero_mat(a.size(), a[0].size());
-    for(int i = 0 ; i < a.size() ; i++)
-        for(int j = 0 ; j < a[0].size() ; j++)
-            res[i][j] = (a[i][j]%MOD + b[i][j]%MOD)%MOD;
+mll mat_add_mod(mll a1, mll a2){
+    mll res = zero_mat(a1.size(), a1[0].size());
+    for(int i = 0 ; i < a1.size() ; i++)
+        for(int j = 0 ; j < a1[0].size() ; j++)
+            res[i][j] = (a1[i][j]%MOD + a2[i][j]%MOD)%MOD;
     return res;
 }
 
 /// --- Multiplication Two Matrix  --- ///
-mll mat_mul(mll a, mll b){
-    mll res = zero_mat(a.size(), b[0].size());
-    for(int i = 0 ; i < a.size() ; i++)
-        for(int k = 0 ; k < a[0].size() ; k++)
-            if( a[i][k] != 0 ) /// --- Optimization --- ///
-                for(int j = 0 ; j < b[0].size() ; j++)
-                    res[i][j] += a[i][k] * b[k][j];
+mll mat_mul(mll a1, mll a2){
+    mll res = zero_mat(a1.size(), a2[0].size());
+    for(int i = 0 ; i < a1.size() ; i++)
+        for(int k = 0 ; k < a1[0].size() ; k++)
+            if( a1[i][k] != 0 ) /// --- Optimization --- ///
+                for(int j = 0 ; j < a2[0].size() ; j++)
+                    res[i][j] += a1[i][k] * a2[k][j];
     return res;
 }
 
 /// --- Multiplication Two Matrix With MOD --- ///
-mll mat_mul_mod(mll a, mll b){
-    mll res = zero_mat(a.size(), b[0].size());
-    for(int i = 0 ; i < a.size() ; i++)
-        for(int k = 0 ; k < a[0].size() ; k++)
-            if( a[i][k] != 0 ) /// --- Optimization --- ///
-                for(int j = 0 ; j < b[0].size() ; j++)
-                    res[i][j] = (res[i][j] + ((a[i][k] % MOD) * (b[k][j] % MOD)) % MOD) % MOD;
+mll mat_mul_mod(mll a1, mll a2){
+    mll res = zero_mat(a1.size(), a2[0].size());
+    for(int i = 0 ; i < a1.size() ; i++)
+        for(int k = 0 ; k < a1[0].size() ; k++)
+            if( a1[i][k] != 0 ) /// --- Optimization --- ///
+                for(int j = 0 ; j < a2[0].size() ; j++)
+                    res[i][j] = (res[i][j] + ((a1[i][k] % MOD) * (a2[k][j] % MOD)) % MOD) % MOD;
     return res;
 }
 
