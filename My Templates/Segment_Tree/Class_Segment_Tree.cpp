e4560996de76f06404408427887a71b357d0abad
Suliman Awad <sulimanawadstudy@gmail.com>
2023-02-24 02:10:28 +0300

    Segment Tree coordinating

--- My Templates/Segment_Tree/Class_Segment_Tree.cpp
+++ My Templates/Segment_Tree/Class_Segment_Tree.cpp
@@ -13,74 +13,55 @@ class SegTree {
     private:
         vector<ll> seg;
         int seg_str, seg_end;
-        ll skip_val = -INF;  /// ----- for max Segment_Tree ----- ///
-        // ll skip_val = INF;  /// ----- for min Segment_Tree ----- ///
 
-        ll conquer(ll a1, ll a2){
-            return max(a1, a2); /// ----- for max Segment_Tree ----- ///
-            // return min(a1, a2); /// ----- for min Segment_Tree ----- ///
-        }
+        ll skip_val = -INF;
+        ll conquer(ll a1, ll a2){ return max(a1, a2); }
 
-        /// params = [root,  first_element,  last_element, base_array]
-        /// params = [ 1  ,       0       ,     n-1      , base_array]
-        void build(int i, int l, int r, ll a[]){
+        void _build(int i, int l, int r, ll a[]){
             if(l == r){
                 seg[i] = a[l];
                 return;
             }
             int m = (l + r)/2;
-            build(LX, l, m, a);
-            build(RX, m+1, r, a);
+            _build(LX, l, m, a);
+            _build(RX, m+1, r, a);
             seg[i] = conquer(seg[LX], seg[RX]);
         }
         
-        /// params = [root,  first_element,  last_element,  index_to_be_updated,  new_value]
-        /// params = [ 1  ,       0       ,     n-1      ,  index_to_be_updated,  new_value]
-        void _update(int i, int l, int r, int idx, ll val){
+        void _update(int idx, ll val, int i, int l, int r){
             if(l == r){
                 seg[i] = val;
                 return;
             }
             int m = (l + r)/2;
-            if(idx <= m) _update(LX, l, m, idx, val);
-            else _update(RX, m+1, r, idx, val);
+            if(idx <= m) _update(idx, val, LX, l, m);
+            else _update(idx, val, RX, m+1, r);
             seg[i] = conquer(seg[LX], seg[RX]);
         }
         
-        /// params = [root,  first_element,  last_element,  from,  to]
-        /// params = [ 1  ,       0       ,     n-1      ,  from,  to]
-        ll _get(int i, int l, int r, int from, int to){
+        ll _get(int from, int to, int i, int l, int r){
             if(from > to) return skip_val;
             if(from == l && to == r) return seg[i];
             int m = (l + r)/2;
-            return conquer(_get(LX, l, m, from, min(to,m)), _get(RX, m+1, r, max(from,m+1), to));
+            return conquer(_get(from, min(to,m), LX, l, m), _get(max(from,m+1), to, RX, m+1, r));
         }
  
-
     public:
-
-        /// params = [first_element,  last_element]
-        SegTree(int str, int end){
+        void resize(int str, int end){
             seg_str = str;
             seg_end = end;
             seg.assign(4*(seg_end-seg_str+1), 0);
         }
 
-        /// params = [first_element,  last_element, base array]
-        SegTree(int str, int end, ll a[]){
-            seg_str = str;
-            seg_end = end;
-            seg.assign(4*(seg_end-seg_str+1), 0);
-            build(1, seg_str, seg_end, a);
+        void build(ll a[]){
+            _build(1, seg_str, seg_end, a);
         }
         
-        /// params = [index_to_be_updated,  new_value]
         void update(int idx, ll val){
-            _update(1, seg_str, seg_end, idx, val);
+            _update(idx, val, 1, seg_str, seg_end);
         }
         
-        /// params = [from,  to]
         ll get(int from, int to){
-            return _get(1, seg_str, seg_end, from, to);
+            return _get(from, to, 1, seg_str, seg_end);
         }
 };
