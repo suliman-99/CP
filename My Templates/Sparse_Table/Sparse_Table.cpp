9c44e8362419c0b975452c00561a44f44299c656
Suliman Awad <sulimanawadstudy@gmail.com>
2023-02-24 05:34:53 +0300

    Create Sparse_Table Template

---
+++ My Templates/Sparse_Table/Sparse_Table.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long       ll;
+typedef unsigned long long       ull;
+typedef vector< ll >    vll;
+typedef vector< vll >   mll;
+
+// -----------------------------------------------------------------
+
+int log2_floor(ull i){ return i ? __builtin_clzll(1) - __builtin_clzll(i) : -1; }
+
+// build in O(nlogn) - get in O(1) - no update
+class SparseTable{
+	private:
+		mll data;
+
+        // min, max, or, and, gcd, ...
+        ll conquer(ll a1, ll a2){ return min(a1, a2); }
+
+		void build(ll arr[]){
+			for(int i = 0 ; i < data[0].size() ; i++) data[0][i] = arr[i];
+			for(int j = 1 ; j < data.size() ; j++)
+				for(int i = 0 ; i < data[j].size() ; i++)
+					data[j][i] = conquer(data[j-1][i], data[j-1][i+(1<<(j-1))]);
+		}
+		
+	public:
+		SparseTable(int sz, ll a[]){
+            data.assign(log2_floor(sz)+1, vll(sz));
+			build(a);
+		}
+		
+		ll get(int i ,int j){
+			int k = log2_floor(j-i+1);
+			return conquer(data[k][i], data[k][j-(1<<k)+1]);
+		}
+};
---
+++ My Templates/Sparse_Table/Sparse_Table_Idx.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long       ll;
+typedef unsigned long long       ull;
+typedef vector<int>       vi;
+typedef vector<vi>        mi;
+
+// -----------------------------------------------------------------
+
+int log2_floor(ull i){ return i ? __builtin_clzll(1) - __builtin_clzll(i) : -1; }
+
+// build in O(nlogn) - get in O(1) - no update
+class SparseTableIdx{
+	private:
+		mi data;
+        ll* arr;
+
+        // min, max, ...
+        int conquer(int idx1, int idx2){ 
+			if(arr[idx1] < arr[idx2]) return idx1; // min
+			// if(arr[idx1] > arr[idx2]) return idx1; // max
+			else return idx2;	
+		}
+
+		void build(){
+			for(int i = 0 ; i < data[0].size() ; i++) data[0][i] = i;
+			for(int j = 1 ; j < data.size() ; j++)
+				for(int i = 0 ; i < data[j].size() ; i++)
+					data[j][i] = conquer(data[j-1][i], data[j-1][i+(1<<(j-1))]);
+		}
+		
+	public:
+		SparseTableIdx(int sz, ll a[]){
+            data.assign(log2_floor(sz)+1, vi(sz));
+            arr = a;
+			build();
+		}
+		
+		int get(int i ,int j){
+			int k = log2_floor(j-i+1);
+			return conquer(data[k][i], data[k][j-(1<<k)+1]);
+		}
+};
