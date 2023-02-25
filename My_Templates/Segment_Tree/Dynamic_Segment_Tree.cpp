48317637063f0d24bf4801466b530a0337054f69
Suliman Awad <sulimanawadstudy@gmail.com>
2023-02-26 02:29:23 +0300

    Create Dynamic Segment Tree Template

--- My_Templates/Segment_Tree/Dynamic_Segment_Tree.cpp
+++ My_Templates/Segment_Tree/Dynamic_Segment_Tree.cpp
@@ -1,70 +1,79 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-struct Node
-{
-  int cnt;
+typedef long long ll;
+const ll INF = 9223372036854775807;
 
-  Node *lf ,*rg;
+// -----------------------------------------------------------------
 
-  Node(): cnt(0) ,lf(nullptr) ,rg(nullptr) {}
+struct Node{
+  ll data;
+  Node *left ,*right;
+  Node(): data(0) ,left(nullptr) ,right(nullptr) {}
 };
 
-map<int , Node*> time_machine;
+// build in O(n) - get in O(logn) - update in O(logn)
+class DySegTree{
+    private:
+        Node *root;
+        int seg_str, seg_end;
 
-void UPD(Node *& cur ,int v ,int t ,int L ,int R)
-{
-  if( !cur ){
-    cur = new Node();
-  }
+        // sum, mul, xor, ... || min, max, or, and, gcd, ...
+        ll skip_val = -INF;
+        ll _conquer(ll a1, ll a2){ return max(a1, a2); }
+        void _update_method(ll &a, ll val){
+            a = val;   // assign
+            // a += val;   // increase
+        }
 
-  if( L == R ){
-    cur -> cnt += v;
-    return;
-  }
+        void _build(Node *& cur, int l, int r, ll a[]){
+            if(!cur){ cur = new Node(); }
+            if(l == r){
+                cur->data = a[l];
+                return;
+            }
+            int m = (l + r)/2;
+            _build(cur->left, l, m, a);
+            _build(cur->right, m+1, r, a);
+            cur->data =_conquer(cur->left->data, cur->right->data);
+        }
+        
+        void _update(int idx, ll val, Node *&cur, int l, int r){
+            if(!cur){ cur = new Node(); }
+            if(l == r){
+                _update_method(cur->data, val);
+                return;
+            }
+            int m = (l + r)/2;
+            if(idx <= m) _update(idx, val, cur->left, l, m);
+            else _update(idx, val, cur->right, m+1, r);
+            cur->data = skip_val;
+            if(cur->left) cur->data = _conquer(cur->data, cur->left->data);
+            if(cur->right) cur->data = _conquer(cur->data, cur->right->data);
+        }
 
-  if( t <= ((L + R) >> 1) ){
-    UPD(cur -> lf ,v ,t ,L ,(L + R) >> 1);
-  } else {
-    UPD(cur -> rg ,v ,t ,((L + R) >> 1) + 1 ,R);
-  }
+        ll _get(int from, int to, Node *cur, int l, int r){
+            if(!cur || to < from) return skip_val;
+            if(from == l && to == r) return cur->data;
+            int m = (l + r)/2;
+            return _conquer(_get(from, min(to,m), cur->left, l, m), _get(max(from,m+1), to, cur->right, m+1, r));
+        }
+ 
+    public:
+        void resize(int str, int end){
+            seg_str = str;
+            seg_end = end;
+        }
 
-  cur -> cnt = 0;
-  if( cur -> lf ) cur -> cnt += cur -> lf -> cnt;
-  if( cur -> rg ) cur -> cnt += cur -> rg -> cnt;
-}
-
-int QRY(Node *cur ,int i ,int j ,int L ,int R)
-{
-  if( !cur || R < i || L > j ) return 0;
-
-  if( i <= L && R <= j ) return cur -> cnt;
-
-  return QRY(cur -> lf ,i ,j ,L ,(L + R) >> 1) + QRY(cur -> rg ,i ,j ,((L + R) >> 1) + 1 ,R);
-}
-
-int main()
-{
-  int q;
-  scanf("%d" ,&q);
-
-  while( q-- )
-  {
-    int a ,t ,x;
-    scanf("%d%d%d" ,&a ,&t ,&x);
-
-    if( !time_machine.count(x) ){
-      time_machine[x] = new Node();
-    }
-
-    if( a == 1 ){
-      UPD(time_machine[x] ,+1 ,t ,1 ,int(1e9));
-    } else if( a == 2 ) {
-      UPD(time_machine[x] ,-1 ,t ,1 ,int(1e9));
-    } else {
-      printf("%d\n" ,QRY(time_machine[x] ,1 ,t ,1 ,int(1e9)));
-    }
-  }
-  return 0;
-}
+        void build(ll a[]){
+            _build(root, seg_str, seg_end, a);
+        }
+        
+        void update(int idx, ll val){
+            _update(idx, val, root, seg_str, seg_end);
+        }
+        
+        ll get(int from, int to){
+            return _get(from, to, root, seg_str, seg_end);
+        }
+};
