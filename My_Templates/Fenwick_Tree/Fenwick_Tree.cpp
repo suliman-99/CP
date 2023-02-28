81674c7cd0418d44bfff171d2ccc7780567714c6
Suliman Awad <sulimanawadstudy@gmail.com>
2023-02-28 22:54:16 +0300

    Fenwick Tree coordinating

--- My_Templates/Fenwick_Tree/Fenwick_Tree.cpp
+++ My_Templates/Fenwick_Tree/Fenwick_Tree.cpp
@@ -9,35 +9,35 @@ typedef vector< ll >    vll;
 // build in O(n) - get in O(logn) - update in O(logn)
 class FenTreePURQ{
 	private:
-		vll data;
+		vll ft;
 
 	public:
-		void resize(int sz){ data.assign(sz+1, 0); }
+		void resize(int sz){ ft.assign(sz+1, 0); }
          
-        void build(ll a[]){
-            for(int i = 1 ; i < data.size() ; i ++){
-                data[i] += a[i-1];
-                if(i+(i&(-i)) < data.size()) data[i+(i&(-i))] += data[i];
+        void build(ll arr[]){
+            for(int i = 1 ; i < ft.size() ; i ++){
+                ft[i] += arr[i-1];
+                if(i+(i&(-i)) < ft.size()) ft[i+(i&(-i))] += ft[i];
             }
         }
 		
 		ll get(int i){
 			ll sum=0; i++;
-			for(;i;i-=(i&(-i))) sum += data[i];
+			for(;i;i-=(i&(-i))) sum += ft[i];
 			return sum;
 		}
 		
 		ll get(int i, int j){ return get(j) - get(i-1); }
 		
-		void update(int i, ll d){ i++; for(;i<data.size();i+=(i&(-i))) data[i] += d; }
+		void update(int i, ll d){ i++; for(;i<ft.size();i+=(i&(-i))) ft[i] += d; }
         
         // find first idx : k <= pre[idx]
 		int lower_bound_on_pre(int k){
 			int p = 1 ,i = 0;
-			while(p*2 < data.size()) p *= 2;
+			while(p*2 < ft.size()) p *= 2;
 			for(;p;p/=2) 
-                if(i+p < data.size() && data[i + p] < k){
-                    k -= data[i + p];
+                if(i+p < ft.size() && ft[i + p] < k){
+                    k -= ft[i + p];
                     i += p;
 				}
 			return i;
@@ -52,7 +52,7 @@ class FenTreeRUPQ{
 	public:
 		void resize(int sz){ purq.resize(sz+1); cpy.assign(sz, 0); }
 
-		void build(ll a[]){ for(int i = 0 ; i < cpy.size() ; i ++) cpy[i] = a[i]; }
+		void build(ll arr[]){ for(int i = 0 ; i < cpy.size() ; i ++) cpy[i] = arr[i]; }
 		
 		void update(int i, int j, ll d){ purq.update(i, +d); purq.update(j+1, -d); }
 		
@@ -68,7 +68,7 @@ class FenTreeRURQ{
 	public:
 		void resize(int sz){ purq.resize(sz+1); rupq.resize(sz); pre.assign(sz+1, 0); }
 		
-		void build(ll a[]){ for(int i = 1 ; i < pre.size() ; i ++) pre[i] = pre[i-1] + a[i-1]; }
+		void build(ll arr[]){ for(int i = 1 ; i < pre.size() ; i ++) pre[i] = pre[i-1] + arr[i-1]; }
 
 		void update(int i ,int j ,ll d){
 			rupq.update(i, j, d);
