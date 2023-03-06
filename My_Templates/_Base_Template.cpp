4bda3c1b0bb8868ade3ee68ed65cdf55025f3079
Suliman Awad <sulimanawadstudy@gmail.com>
2023-03-06 04:52:32 +0300

    Base Template

--- My_Templates/_Base_Template.cpp
+++ My_Templates/_Base_Template.cpp
@@ -13,7 +13,6 @@ void test(){
 // -----------------------------------------------------------------
 
 #define FastIO        cin.tie(0);cout.tie(0);ios::sync_with_stdio(0);
-#define printv(v)     for(auto x:v){cout<<x<<" ";}cout<<endl;
 #define dbg(x)        cout<<#x<<" = "<<x<<endl;
 #define SQ(a)         (a)*(a)
 #define PB            push_back
@@ -57,27 +56,10 @@ const ld PI  = 3.141592653589793238;
 const int TOW = 0b10;
 const int FIFTEEN = 0xf;
 
-int tc = 1;
-
 // -----------------------------------------------------------------
 
-const int N = 3e5 + 5e3;
-
-ll n, a[N];
-
-
-// --------------------------------------
-
-
-
-// --------------------------------------
 
 void doTest(){
-    cin>>n;
-    for(int i = 0 ; i < n ; i ++){
-        cin>>a[i];
-    }
-
 
 }
 
@@ -89,9 +71,13 @@ int main(){
     FastIO
     cout<<fixed<<setprecision(15);
 
+    int tc = 1, tt = 0;
     // scanf("%d", &tc);
     cin>>tc;
-    while(tc--) doTest();
+    while(tt++ < tc){   
+		cout<<"Case #"<<tt<<": ";
+        doTest();
+    }
 }
 
 
