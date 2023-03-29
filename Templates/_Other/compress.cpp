914e4f8d5b87f2f27e66a9915a49744106828cd7
Suliman Awad <sulimanawadstudy@gmail.com>
2023-03-29 04:57:38 +0300

    Add map_compress to compress file.

--- Templates/_Other/compress.cpp
+++ Templates/_Other/compress.cpp
@@ -24,6 +24,7 @@ int compress(ll source[],int sz){
     return mx;
 }
 
+// compress the source array & return max element
 int compress(vll source){
     ii temp[source.size()]; int mx = 0;
     for(int i = 0 ; i < source.size() ; i ++){
@@ -36,4 +37,21 @@ int compress(vll source){
         if(i < source.size()-1 && temp[i].F != temp[i+1].F) mx++;
     }
     return mx;
+}
+
+
+// -------------------------------------------
+// there is another technique of compressing
+// make a map and fill it ( a[i]: i)
+// then use map[a[i]] instead of a[i]
+// note: i is the idx idx if we put the elements in a set
+// note: i is not the idx in the array
+
+map<ll,ll> compressor;
+
+void map_compress(vll source){
+    int idx = 0;
+    for(int i = 0 ; i < source.size() ; i ++)
+        if(compressor.count(source[i] == 0))
+            compressor[source[i]] = idx++;
 }
